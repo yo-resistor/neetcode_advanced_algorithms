dd9ca70450f0779acad0ec3df3d616a589d932b1
Yunsik Ohm <yunsikohm@gmail.com>
2024-10-28 08:33:17 -0700

    brute_force.py: done with scripting

--- 00_kadanes_algorithm/brute_force.py
+++ 00_kadanes_algorithm/brute_force.py
@@ -1,9 +1,18 @@
 def brute_force(nums):
-    return 0
+    # this method is using two pointers
+    max_sum = nums[0]
+    
+    for i in range(len(nums)):
+        cur_sum = 0
+        for j in range(i, len(nums)):
+            cur_sum += nums[j]
+            max_sum = max(max_sum, cur_sum)
+            
+    return max_sum
 
 def main():
     nums = [4, -1, 2, -7, 3, 4]
-    print(brute_force)
+    print(brute_force(nums))
     
 if __name__ == "__main__":
     main()
