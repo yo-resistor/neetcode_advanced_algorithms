ac71fca4addd1176427b5b73eaa5169538a49b15
Yunsik Ohm <yunsikohm@gmail.com>
2024-10-29 08:34:40 -0700

    leetcode 978: solved after watching neetcode solution

--- 00_kadanes_algorithm/978_longest_turbulent_subarray.py
+++ 00_kadanes_algorithm/978_longest_turbulent_subarray.py
@@ -2,8 +2,29 @@ from typing import List
 
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
+        L, R = 0, 1
+        max_count = 1
+        count = 1
+        prev = ""
         
-        return 0
+        while R < len(arr):
+            if arr[R - 1] > arr[R] and prev != ">":
+                count += 1
+                max_count = max(count, max_count)
+                R += 1
+                prev = ">"
+            elif arr[R - 1] < arr[R] and prev != "<":
+                count += 1
+                max_count = max(count, max_count)
+                R += 1
+                prev = "<"    
+            else:
+                R = R + 1 if arr[R] == arr[R - 1] else R
+                L = R - 1
+                count = 1
+                prev = ""
+        
+        return max_count
     
 def main():
     sol = Solution()
