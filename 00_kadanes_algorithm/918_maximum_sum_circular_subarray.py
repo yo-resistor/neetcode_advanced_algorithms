5e234ec81d98cfc157523d422ba0acab6761e80d
Yunsik Ohm <yunsikohm@gmail.com>
2024-10-29 21:52:55 -0700

    leetcode 918: solved

--- 00_kadanes_algorithm/918_maximum_sum_circular_subarray.py
+++ 00_kadanes_algorithm/918_maximum_sum_circular_subarray.py
@@ -2,8 +2,18 @@ from typing import List
 
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        curr_max, curr_min = 0, 0
+        glob_max, glob_min = nums[0], nums[0]
+        total = 0
         
-        return 0
+        for num in nums:
+            total += num
+            curr_max = max(curr_max + num, num)
+            curr_min = min(curr_min + num, num)
+            glob_max = max(glob_max, curr_max)
+            glob_min = min(glob_min, curr_min)
+            
+        return max(glob_max, total - glob_min) if glob_max > 0 else glob_max
     
 def main():
     sol = Solution()
