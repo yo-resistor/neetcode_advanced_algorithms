3f780b10b57ec0ec3d858f9591c694af79644bd4
Yunsik Ohm <yunsikohm@gmail.com>
2024-11-15 09:26:13 -0800

    leetcode 212: solved with hint using dfs

--- 06_trie/212_word_search_ii.py
+++ 06_trie/212_word_search_ii.py
@@ -1,49 +1,64 @@
 from typing import List
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+    
+    def add_word(self, word):
+        curr = self
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.is_end = True
+
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        ROWS = len(board)
-        COLS = len(board[0])
-        result = []
+        # define a trie and add words in the trie
+        root = TrieNode()
+        for word in words: 
+            root.add_word(word)
         
-        def search(idx, row, col, board, check, word):
-            # if the element is not inside the board
+        # run dfs to search word stored in the trie
+        ROWS, COLS = len(board), len(board[0])
+        result, visit = set(), set()
+        
+        # define dfs function
+        def dfs(row, col, curr_node, word_history):
+            # define the base case to exit dfs function
             if (
                 row < 0 or row >= ROWS or
-                col < 0 or col >= COLS
+                col < 0 or col >= COLS or
+                (row, col) in visit or
+                board[row][col] not in curr_node.children
             ):
-                return False
+                return
             
-            # if the idx is out of word
-            # we succeeded
-            if idx >= len(word) or idx < 0:
-                return True
+            # go deeper by 1 step
+            curr_node = curr_node.children[board[row][col]]
+            word_history += board[row][col]
+            visit.add((row, col))
             
-            print(f"interest: {word[idx]} / on board: {board[row][col]}")
-            # if we find the element in the board
-            # and it was not explored before
-            if word[idx] == board[row][col] and check[row][col] == 0:
-                print("found")
-                check[row][col] = 1
-                return (
-                    search(idx + 1, row + 1, col, board, check, word) or
-                    search(idx + 1, row - 1, col, board, check, word) or
-                    search(idx + 1, row, col + 1, board, check, word) or
-                    search(idx + 1, row, col - 1, board, check, word)
-                )
+            # search is completed
+            if curr_node.is_end:
+                result.add(word_history)
+            
+            # run dfs in 4 direction: up, down, left, right
+            dfs(row + 1, col, curr_node, word_history)      # down
+            dfs(row - 1, col, curr_node, word_history)      # up
+            dfs(row, col + 1, curr_node, word_history)      # right
+            dfs(row, col - 1, curr_node, word_history)      # left
+            
+            # take 1 step back
+            visit.remove((row, col))
         
-        for word in words:
-            print(f"target: [{word}]")
-            word_idx = 0
-            for row in range(ROWS):
-                if word[word_idx] in board[row]:
-                    for col in range(COLS):
-                        if word[word_idx] == board[row][col]:
-                            board_check = [[0 for j in range(COLS)] for i in range(ROWS)]
-                            if search(word_idx, row, col, board, board_check, word):
-                                result.append(word)
+        # visit each element in the board and run dfs
+        for row in range(ROWS):
+            for col in range(COLS):
+                dfs(row, col, root, "")
         
-        return result
+        return list(result)
 
 def main():
     sol = Solution()
